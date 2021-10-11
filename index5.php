2c27cbd5276ed82efe7997529e5d4e94f795481a
alberto <albertozanola1996@gmail.com>
2021-10-11 16:04:34 +0200

    Completamento Snack-5

--- index5.php
+++ index5.php
@@ -1,5 +1,11 @@
 <!-- Prendere un paragrafo abbastanza lungo, contenente diverse frasi. Prendere il paragrafo e suddividerlo in tanti paragrafi. Ogni punto un nuovo paragrafo. -->
+<?php
 
-
-
+    $testo = 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus, accusamus. Dolorum doloribus deserunt. quas quasi assumenda ab fugiat amet. recusandae magnam quam error numquam sunt explicabo! Molestias eveniet neque architecto?';
+    $frasi = explode('.', $testo);
+    echo $testo . '<br/>';
+    foreach ($frasi as $key => $frase) {
+        echo '<br/>' . $frase . '<br/>';
+    }
+?>
 
