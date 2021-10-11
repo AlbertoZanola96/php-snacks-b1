2031651fffd747bf4d534ce9dd22fee6a08c8554
alberto <albertozanola1996@gmail.com>
2021-10-11 15:53:04 +0200

    Completamento Snack-4

--- index4.php
+++ index4.php
@@ -1,2 +1,11 @@
 <!-- Creare un array con 15 numeri casuali ( da 1 a 100), tenendo conto che l’array non dovrà contenere lo stesso numero più di una volta  -->
-
+<?php 
+    $numeri = [];
+    while (count($numeri) < 15) {
+        $genNumero = rand(1,100);
+        if(in_array($genNumero, $numeri) === false) {
+            $numeri[] = $genNumero;
+        }
+    }
+    var_dump($numeri);
+?> 
